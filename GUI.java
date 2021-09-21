fa0adc9be64e37df7e613fb02912a1909f6c586e
hiba127 <iich.hiba@gmail.com>
2021-09-20 21:06:24 -0400

    Update GUI.java

--- GUI.java
+++ GUI.java
@@ -1,3 +1,13 @@
-public class GUI {
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class GUI extends JFrame implements ActionListener {
+
+    SortingArray arraylist = new SortingArray();
+
+    JTextField t1 = new JTextField(30);
+
+    JTextArea inst = new JTextArea("Write your numbers"
     
 }
