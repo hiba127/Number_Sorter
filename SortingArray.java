70c7d4a1078ab992eb2afb88af574201a8b85dbd
hiba127 <iich.hiba@gmail.com>
2021-09-20 21:05:47 -0400

    Create SortingArray.java

---
+++ SortingArray.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class SortingArray {
+
+    public SortingArray(){
+
+    ArrayList<Integer> arr = new ArrayList<Integer>();
+    }
+
+    public void newNum(int n){
+        arr.add(n);
+    }
+
+    public void display(){
+
+    }
+
+
+}
